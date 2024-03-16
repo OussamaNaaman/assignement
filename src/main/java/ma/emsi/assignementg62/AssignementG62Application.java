fb7e30982ee504ee149ce9b78bc878790d68936a
Naaman <naamanoussama03@gmail.com>
2024-03-16 11:22:37 +0000

    premier pas

---
+++ src/main/java/ma/emsi/assignementg62/AssignementG62Application.java
@@ -0,0 +1,13 @@
+package ma.emsi.assignementg62;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignementG62Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignementG62Application.class, args);
+    }
+
+}
